2278f3d1d34231da2b89f1b9e61eddf848f2c220
FangYu199 <talentyupush@163.com>
2020-11-12 17:12:01 +0800

    update

---
+++ precache-manifest.5f07906827cfbbb865f9720a2f7a208e.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ba2f83e2447466f3d218b5efd54083",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6d38e70f76e65f8fba19",
+    "url": "./static/css/main.949dd16a.chunk.css"
+  },
+  {
+    "revision": "933d16fe332ea6e2b8ef",
+    "url": "./static/js/0.4fa00ba2.chunk.js"
+  },
+  {
+    "revision": "70c54901b66f2ac129d6",
+    "url": "./static/js/10.a723c4fd.chunk.js"
+  },
+  {
+    "revision": "482e53dfa96f97b26686",
+    "url": "./static/js/11.1f2fb48a.chunk.js"
+  },
+  {
+    "revision": "4a50ab7b49b6e897e5ed",
+    "url": "./static/js/3.2f0bf4c3.chunk.js"
+  },
+  {
+    "revision": "15723851b11509ef1b9a8dbd2a0ef589",
+    "url": "./static/js/3.2f0bf4c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eedbdf55f2ea23ca1bca",
+    "url": "./static/js/4.d548b9da.chunk.js"
+  },
+  {
+    "revision": "db80bce0046b956884b2",
+    "url": "./static/js/5.213c1163.chunk.js"
+  },
+  {
+    "revision": "374658371e5d9c95e00ea15067d24dad",
+    "url": "./static/js/5.213c1163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02ae5e26f0c86a2de351",
+    "url": "./static/js/6.79963e43.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "./static/js/6.79963e43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69e90bfc06e8232742a7",
+    "url": "./static/js/7.870be820.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "./static/js/7.870be820.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5552255b2b47c4fa6c5",
+    "url": "./static/js/8.0690de2c.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "./static/js/8.0690de2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53ffe59e1ea8f5bb1994",
+    "url": "./static/js/9.8f8a6326.chunk.js"
+  },
+  {
+    "revision": "6d38e70f76e65f8fba19",
+    "url": "./static/js/main.201f9404.chunk.js"
+  },
+  {
+    "revision": "aeaac641d84c9992df43",
+    "url": "./static/js/runtime-main.b566f97a.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "./static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "bcf2e9a6b54739f7d8d4cfcad7a32677",
+    "url": "./static/media/bottom.bcf2e9a6.png"
+  },
+  {
+    "revision": "4af50c96ded8d36316fb8289b612dcec",
+    "url": "./static/media/icon1.4af50c96.ico"
+  },
+  {
+    "revision": "57b97c35295643633c275fc5453d5d21",
+    "url": "./static/media/icon2.57b97c35.ico"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "./static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "37f2bc6ecee2eb29b3cf6799ce58bf58",
+    "url": "./static/media/wallet-connect.37f2bc6e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.af4ef17292722613e480571ed3d9d434.js"
+  "./precache-manifest.5f07906827cfbbb865f9720a2f7a208e.js"
 );
 
 self.addEventListener('message', (event) => {
