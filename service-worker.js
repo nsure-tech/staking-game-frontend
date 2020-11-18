d01c269bc1c56af84e26858345d284bb65270967
FangYu199 <talentyupush@163.com>
2020-11-18 11:29:07 +0800

    update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3904ad8ba52ffa22f6ab2bc6d049201c.js"
+  "./precache-manifest.ffb640b031f9e6ab2c808aaa522b50a6.js"
 );
 
 self.addEventListener('message', (event) => {
